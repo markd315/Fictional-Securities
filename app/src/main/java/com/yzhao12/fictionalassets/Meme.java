490551cf71d4b156ef03c9fb96633d49b324e376
yzhao12 <yangzhao12@gmail.com>
2017-06-16 19:17:43 -0700

    finished preliminary Meme class/data storage type.

--- app/src/main/java/com/yzhao12/fictionalassets/Meme.java
+++ app/src/main/java/com/yzhao12/fictionalassets/Meme.java
@@ -9,11 +9,49 @@ public class Meme {
 
     }
 
+    public Meme(String name, float[] historicalPrices, int issuedShares, String exampleUrl) {
+        m_name = name;
+        m_historicalPrices = historicalPrices;
+        m_issuedShares = issuedShares;
+        m_exampleUrl = exampleUrl;
+    }
+
+    public String getName() {
+        return m_name;
+    }
+
+    public void setName(String name) {
+        m_name = name;
+    }
+
+    public float[] getHistoricalPrices() {
+        return m_historicalPrices;
+    }
+
+    public void setHistoricalPrices(float[] historicalPrices) {
+        m_historicalPrices = historicalPrices;
+    }
+
+    public int getIssuedShares() {
+        return m_issuedShares;
+    }
+
+    public void setIssuedShares(int issuedShares) {
+        m_issuedShares = issuedShares;
+    }
+
+    public String getExampleUrl() {
+        return m_exampleUrl;
+    }
+
+    public void setExampleUrl(String exampleUrl) {
+        m_exampleUrl = exampleUrl;
+    }
 
 
 
-
-    private String name;
-    private float[] historicalPrices;
-    private String issuedShares;
+    private String m_name;
+    private float[] m_historicalPrices;
+    private int m_issuedShares;
+    private String m_exampleUrl;
 }
