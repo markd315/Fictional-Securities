0213454d85a41cba7a917f591253dcd9c1c6a16c
yzhao12 <yangzhao12@gmail.com>
2017-06-17 20:39:48 -0400

    finished preliminary User class/data type

--- app/src/main/java/com/yzhao12/fictionalassets/User.java
+++ app/src/main/java/com/yzhao12/fictionalassets/User.java
@@ -1,5 +1,6 @@
 package com.yzhao12.fictionalassets;
 
+import android.net.Uri;
 import com.google.firebase.auth.FirebaseUser;
 
 /**
@@ -7,15 +8,53 @@ import com.google.firebase.auth.FirebaseUser;
  */
 
 public class User {
-
     public User(FirebaseUser userInstance) {
-        m_userInstance = userInstance;
+        m_userName = userInstance.getDisplayName();
+        m_email = userInstance.getEmail();
+        m_profilePicUrl = userInstance.getPhotoUrl();
+        m_uid = userInstance.getUid();
+    }
+
+    public String getM_userName() {
+        return m_userName;
+    }
+
+    public void setM_userName(String userName) {
+        m_userName = userName;
+    }
+
+    public String getM_email() {
+        return m_email;
+    }
+
+    public void setM_email(String email) {
+        m_email = email;
+    }
+
+    public Uri getM_profilePicUrl() {
+        return m_profilePicUrl;
+    }
+
+    public void setM_profilePicUrl(Uri profilePicUrl) {
+        m_profilePicUrl = profilePicUrl;
+    }
+
+    public String getM_uid() {
+        return m_uid;
+    }
+
+    public void setM_uid(String uid) {
+        m_uid = uid;
     }
 
 
 
-
-
     private FirebaseUser m_userInstance;
+
+    //Might need to extrace string from Firebase.Promise
+    private String m_userName;
+    private String m_email;
+    private Uri m_profilePicUrl;
+    private String m_uid;
 
 }
