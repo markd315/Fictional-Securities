0baa2875248aa8b720000d15eae0879df8515cc5
yzhao12 <yangzhao12@gmail.com>
2017-06-18 23:27:25 -0400

    member variables for Transaction

--- app/src/main/java/com/yzhao12/fictionalassets/Transaction.java
+++ app/src/main/java/com/yzhao12/fictionalassets/Transaction.java
@@ -1,9 +1,19 @@
 package com.yzhao12.fictionalassets;
 
+import java.sql.Time;
+
 /**
  * Created by Yang on 6/18/2017.
  */
 
 public class Transaction {
 
+
+
+
+
+    private int m_shares;
+    private float m_price;
+    private Time m_timestamp;
+    private int m_user;
 }
