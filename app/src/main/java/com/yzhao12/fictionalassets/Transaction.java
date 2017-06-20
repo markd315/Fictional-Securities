7314b90b1db871ac510d855810c7eb2f4d1479c5
yzhao12 <yangzhao12@gmail.com>
2017-06-19 21:49:32 -0400

    finished Transaction class/data type

--- app/src/main/java/com/yzhao12/fictionalassets/Transaction.java
+++ app/src/main/java/com/yzhao12/fictionalassets/Transaction.java
@@ -15,7 +15,37 @@ public class Transaction {
         m_userid = userid;
     }
 
+    public int getShares() {
+        return m_shares;
+    }
 
+    public void setShares(int shares) {
+        m_shares = shares;
+    }
+
+    public float getPrice() {
+        return m_price;
+    }
+
+    public void setPrice(float price) {
+        m_price = price;
+    }
+
+    public Time getM_timestamp() {
+        return m_timestamp;
+    }
+
+    public void setTimestamp(Time timestamp) {
+        m_timestamp = timestamp;
+    }
+
+    public int getM_userid() {
+        return m_userid;
+    }
+
+    public void setUserid(int userid) {
+        m_userid = userid;
+    }
 
 
     private int m_shares;
