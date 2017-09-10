b0a3977a0f665c6eadbca8543b7e2866c42e8bce
yzhao12 <yangzhao12@gmail.com>
2017-09-10 18:27:19 -0400

    added searchable.xml and an empty SearchActivity with its layouts (activity_search.xml and search_page.xml)

---
+++ app/src/main/java/com/yzhao12/fictionalassets/SearchActivity.java
@@ -0,0 +1,14 @@
+package com.yzhao12.fictionalassets;
+
+import android.app.ListActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
