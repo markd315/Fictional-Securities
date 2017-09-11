3b76e827016cabf2c2739fbd79c925ebd3f88b65
yzhao12 <yangzhao12@gmail.com>
2017-09-10 23:08:59 -0400

    added search widget button in toolbar, must complete search, display meme, and part of buy and sell tomorrow

--- app/src/main/java/com/yzhao12/fictionalassets/SearchActivity.java
+++ app/src/main/java/com/yzhao12/fictionalassets/SearchActivity.java
@@ -3,6 +3,7 @@ package com.yzhao12.fictionalassets;
 import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class SearchActivity extends ListActivity {
 
@@ -10,5 +11,25 @@ public class SearchActivity extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            case R.id.action_favorite:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
